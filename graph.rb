f451248c647688274c597c0f05f3cfa1cdc3c08a
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-07-17 17:49:58 -0300

    Use of sets instead of hashmaps in Edge

--- graph.rb
+++ graph.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class Edge
     def initialize(u, v)
         @u, @v = u, v
@@ -7,15 +9,19 @@ end
 class Vertex
     def initialize(symbol)
         @symbol = symbol
-        @edges = Hash.new
+        @edges = Set.new
     end
 
     def symbol()
         @symbol
     end
 
+    def edges()
+        @edges
+    end
+
     def insert_edge(v)
-        @edges[v.symbol] = Edge.new(@symbol, v.symbol)
+        @edges.add(Edge.new(@symbol, v.symbol))
     end
 end
 
@@ -42,7 +48,7 @@ v2.insert_edge(v1)
 vertices = {:'New York' => v1, :'London' => v2}
 
 graph = Graph.new(vertices)
-# graph.insert_vertex(v1)
-# graph.insert_vertex(v2)
+graph.insert_vertex(v1)
+graph.insert_vertex(v2)
 
 puts graph.vertices
