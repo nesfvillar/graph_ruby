b4b51824ed0ed2266860af9d9b2ff670f4c73b0e
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-07-18 20:33:28 -0300

    Changed way of adding edges

--- graph.rb
+++ graph.rb
@@ -25,20 +25,16 @@ class Vertex
         @edges = Set.new
     end
 
-    def insert_edge(vertex)
-        @edges.add(Edge.new(@symbol, vertex.symbol)) unless @edges.include?(find_edge(vertex))
-    end
-
-    def insert_weighted_edge(vertex, weight)
-        @edges.add(WeightedEdge.new(@symbol, vertex.symbol, weight)) unless @edges.include?(find_edge(vertex))
+    def insert_edge(edge)
+        @edges.add(edge)
     end
 
     def find_edge(vertex)
         @edges.detect {|edge| edge.v === vertex.symbol}
     end
 
-    def delete_edge(vertex)
-        @edges.delete(self.find_edge(vertex))
+    def delete_edge(edge)
+        @edges.delete(edge)
     end
 end
 
@@ -81,7 +77,7 @@ class Graph
 
     def dfs(start, finish)
         to_explore = Array.new.push start
-        seen = Set.new
+        seen = Set.new.add start
         while !to_explore.empty?
             current = to_explore.pop
             if current === finish
@@ -101,19 +97,18 @@ end
 v1 = Vertex.new(:'New York')
 v2 = Vertex.new(:'London')
 v3 = Vertex.new(:'Paris')
+v4 = Vertex.new(:'Tokyo')
+v5 = Vertex.new(:'Buenos Aires')
 
-v1.insert_edge(v2)
-v2.insert_edge(v1)
-
-v1.insert_weighted_edge(v3, 100)
-v3.insert_weighted_edge(v1, 100)
-
-v2.insert_weighted_edge(v3, 5)
-v3.insert_weighted_edge(v2, 5)
+v1.insert_edge(Edge.new(v1, v2))
+v2.insert_edge(Edge.new(v2, v3))
+v3.insert_edge(Edge.new(v3, v4))
+v4.insert_edge(Edge.new(v4, v5))
 
 graph = Graph.new()
 graph.insert_vertex(v1)
 graph.insert_vertex(v2)
 graph.insert_vertex(v3)
 
-puts graph.vertices
+puts graph.bfs(v1, v5)
+puts graph.dfs(v1, v5)
